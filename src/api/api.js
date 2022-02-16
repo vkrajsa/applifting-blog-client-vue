2fb804ee78fda911f25c4a37761d93f1f407e5c7
vkrajsa <vkrajsa@gmail.com>
2022-02-16 15:49:32 +0100

    Create axios instance for server api calls

---
+++ src/api/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.VUE_APP_ROOT_API,
+  headers: {
+    Accept: 'application/json',
+    'X-API-KEY': process.env.VUE_APP_API_KEY,
+    'Content-Type': 'application/json',
+  },
+});
+
+export default api;
