8fab8f29af1670c08773e008ccc894af0b981884
vkrajsa <vkrajsa@gmail.com>
2022-02-16 15:28:05 +0100

    Add routes to navigation

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,8 +1,11 @@
 <template>
   <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light" role="navigation">
-      <router-link to="/articles" class="nav-link">Articles</router-link>
-      <router-link to="/login" class="nav-link">Login</router-link>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light container-fluid" role="navigation">
+      <router-link to="/articles" class="nav-link">Recent Articles</router-link>
+      <router-link to="/my-articles" class="nav-link">My Articles</router-link>
+      <router-link to="/add-article" class="nav-link">Create Article</router-link>
+      <router-link to="/login" class="nav-link ms-auto">Login</router-link>
+      <button class="btn btn-primary">Log-out</button>
     </nav>
   </header>
 </template>
