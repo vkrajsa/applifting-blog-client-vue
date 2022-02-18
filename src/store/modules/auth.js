f86d92bddd2993a934593310ea59bdb97afdec47
vkrajsa <vkrajsa@gmail.com>
2022-02-18 18:34:20 +0100

    Save access token in localStorage and add to the axios default headers

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,17 +1,29 @@
+import axios from 'axios';
 import { userLogin } from '@/api/user';
 
 const state = () => ({
-  user: null,
+  token: null,
 });
+
+const mutations = {
+  setToken(state, userToken) {
+    state.token = true;
+    localStorage.setItem('userToken', JSON.stringify(userToken));
+    axios.defaults.headers.common['Authorization'] = `Bearer ${userToken.access_token}`;
+  },
+};
 
 const actions = {
   async logIn({ commit }, credentials) {
     const response = await userLogin(credentials);
+    const userToken = response.data;
+    commit('setToken', userToken);
   },
 };
 
 export default {
   namespaced: true,
   state,
+  mutations,
   actions,
 };
