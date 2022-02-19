ecbd770c012a11f8c9e7b38806b0b0ecb7d2661c
vkrajsa <vkrajsa@gmail.com>
2022-02-19 14:26:37 +0100

    Hide navigation routes for unauthenticated user

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -18,8 +18,8 @@ export default {
   <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light container-fluid" role="navigation">
       <router-link to="/articles" class="nav-link">Recent Articles</router-link>
-      <router-link to="/my-articles" class="nav-link">My Articles</router-link>
-      <router-link to="/add-article" class="nav-link">Create Article</router-link>
+      <router-link v-if="isLoggedIn" to="/my-articles" class="nav-link">My Articles</router-link>
+      <router-link v-if="isLoggedIn" to="/add-article" class="nav-link">Create Article</router-link>
       <router-link v-if="!isLoggedIn" to="/login" class="nav-link ms-auto">Login</router-link>
       <button v-else class="btn btn-primary" @click="logOut()">Log-out</button>
     </nav>
