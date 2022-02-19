9d8b7b35e50c17d355e0edd63f0060e2f66b68df
vkrajsa <vkrajsa@gmail.com>
2022-02-19 13:38:50 +0100

    Prevent user from being logged out after refreshing browser

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-import { createApp } from 'vue';
+import { createApp, h } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-createApp(App).use(store).use(router).mount('#app');
+const app = createApp({
+  created() {
+    const userToken = localStorage.getItem('userToken');
+
+    if (userToken) {
+      this.$store.commit('auth/setToken', JSON.parse(userToken));
+    }
+  },
+  render: () => h(App),
+});
+
+app.use(store).use(router);
+
+app.mount('#app');
