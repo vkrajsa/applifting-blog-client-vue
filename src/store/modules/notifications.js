7da2920036a99e60913a2fa5f17bdedc3ed0290f
vkrajsa <vkrajsa@gmail.com>
2022-02-20 12:22:23 +0100

    Add notifications module for global error notifications

---
+++ src/store/modules/notifications.js
@@ -0,0 +1,38 @@
+const state = {
+  notifications: [],
+};
+
+let nextId = 1;
+
+const mutations = {
+  pushNotification(state, notification) {
+    // maximum notifications on screen set to 3
+    if (state.notifications.length > 3) {
+      state.notifications.shift();
+    }
+    state.notifications.push({
+      id: nextId++,
+      ...notification,
+    });
+  },
+  removeNotification(state, notificationToRemove) {
+    state.notifications = state.notifications.filter((notification) => notification.id !== notificationToRemove.id);
+  },
+};
+
+const actions = {
+  add({ commit }, notification) {
+    console.log(notification);
+    commit('pushNotification', notification);
+  },
+  remove({ commit }, notification) {
+    commit('removeNotification', notification);
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+};
