c08e2d8932d6cd663bcb7d3d378c3db029d1dcba
vkrajsa <vkrajsa@gmail.com>
2022-02-20 12:22:49 +0100

    Create Notification helper class

--- src/store/helpers.js
+++ src/store/helpers.js
@@ -3,3 +3,17 @@ import { mapGetters } from 'vuex';
 export const isAuthenticated = {
   ...mapGetters('auth', ['isLoggedIn']),
 };
+
+export class Notification {
+  constructor(response) {
+    this.title = response.data.title || response.data.message;
+    this.status = response.status;
+
+    this.notificationType = () => {
+      if (this.status >= 400) {
+        return 'error';
+      }
+      return 'success';
+    };
+  }
+}
