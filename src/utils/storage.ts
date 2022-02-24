c89aefc6816df53b0bea9c546447ff1155e0204b
vkrajsa <vkrajsa@gmail.com>
2022-02-24 19:32:21 +0100

    Add user storage class

---
+++ src/utils/storage.ts
@@ -0,0 +1,25 @@
+export default class Storage<T = unknown> {
+  private readonly key: string;
+
+  constructor(key: string) {
+    this.key = key;
+  }
+
+  get() {
+    try {
+      const token = localStorage.getItem(this.key) ?? '';
+      return !!token;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  set(token: T): void {
+    const parsedToken = JSON.stringify(token);
+    localStorage.setItem(this.key, parsedToken);
+  }
+
+  remove(): void {
+    localStorage.removeItem(this.key);
+  }
+}
