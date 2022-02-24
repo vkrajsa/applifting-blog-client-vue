5d5e617637a20eb2cee267a6e0e9789256d64215
vkrajsa <vkrajsa@gmail.com>
2022-02-24 12:02:00 +0100

    Make prettier work with typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,23 +5,23 @@ module.exports = {
     browser: true,
     node: true,
   },
-
-  extends: ['plugin:vue/vue3-recommended', 'prettier'],
-
+  parser: 'vue-eslint-parser',
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    'prettier',
+    '@vue/typescript',
+    '@vue/typescript/recommended',
+  ],
   parserOptions: {
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
     allowImportExportEverywhere: true,
   },
-
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-multiple-template-root': 'off',
   },
-
-  'extends': [
-    'plugin:vue/vue3-recommended',
-    'prettier',
-    '@vue/typescript'
-  ]
 };
