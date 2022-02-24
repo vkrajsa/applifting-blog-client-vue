f519f137a38ecde62928bf3b8f8a1cc1fa922d0d
vkrajsa <vkrajsa@gmail.com>
2022-02-24 19:39:31 +0100

    Set autorization header on createApp if user is logged in

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import { createApp, h } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store/index';
+import { setAuthorizationHeader } from './utils/auth';
+import { userStorage } from './store/modules/user';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const app = createApp({
+  created() {
+    if (userStorage.get()) {
+      setAuthorizationHeader();
+    }
+  },
   render: () => h(App),
 });
 
