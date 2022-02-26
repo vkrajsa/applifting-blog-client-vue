2f2fa2b649f04e172ce33936ff82579d384c42a5
vkrajsa <vkrajsa@gmail.com>
2022-02-26 15:34:27 +0100

    Add app link component

---
+++ src/components/AppLink.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useArticles } from '../composable/useArticles';
+import ArticlePreview from '../components/ArticlePreview.vue';
+import { AppRouteNames, RouteParams } from '../types/routes';
+import { useAttrs } from 'vue';
+
+interface AppLink {
+  name: AppRouteNames;
+  params?: RouteParams;
+}
+
+const props = defineProps<AppLink>();
+</script>
+
+<template>
+  <router-link :to="props" v-bind="attrs">
+    <slot />
+  </router-link>
+</template>
---
+++ src/types/routes.ts
@@ -0,0 +1,10 @@
+import type { RouteParams } from 'vue-router';
+
+export type AppRouteNames =
+  | 'index'
+  | 'articles'
+  | 'article-detail'
+  | 'login'
+  | 'articles-admin'
+  | 'article-post'
+  | 'article-edit';
