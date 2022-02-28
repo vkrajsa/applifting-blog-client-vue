6b6cbab5a0ebe0704aff2d1829b8a24a7eb131ec
vkrajsa <vkrajsa@gmail.com>
2022-02-28 15:20:14 +0100

    Connect markdown component with article form, create reactive form

--- src/components/ArticleForm.vue
+++ src/components/ArticleForm.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
+import { reactive } from 'vue';
+import { PostArticle } from '@/types/article';
 import MarkdownEditor from '../components/MarkdownEditor.vue';
+import BaseInput from '../components/base/BaseInput.vue';
+
+const form = reactive<PostArticle>({
+  title: '',
+  content: '',
+  perex: null,
+  imageId: null,
+});
 </script>
 
 <template>
-  <MarkdownEditor> </MarkdownEditor>
+  <form @submit.prevent="postForm">
+    <BaseInput v-model="form.title" label="Title" type="text" required />
+    <MarkdownEditor v-model="form.content"> </MarkdownEditor>
+  </form>
 </template>
--- src/components/MarkdownEditor.vue
+++ src/components/MarkdownEditor.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const text = ref();
+</script>
 
 <template>
   <v-md-editor v-model="text" height="400px"></v-md-editor>
