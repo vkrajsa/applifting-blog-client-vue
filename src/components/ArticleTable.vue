86fa42ba3cbf457075d7c57d7917eca0b498fd3b
vkrajsa <vkrajsa@gmail.com>
2022-03-01 10:16:25 +0100

    Add articles table

---
+++ src/components/ArticleTable.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useArticles } from '../composable/useArticles';
+import ArticleTableRow from './ArticleTableRow.vue';
+
+const { articles, fetchArticles } = await useArticles();
+
+await fetchArticles();
+</script>
+
+<template>
+  <div class="table-responsive">
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Article title</th>
+          <th scope="col">Perex</th>
+          <th scope="col">Author</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+      <ArticleTableRow v-for="article in articles" :key="article.articleId" :article="article" />
+    </table>
+  </div>
+</template>
