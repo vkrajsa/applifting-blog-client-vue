b27177fb415aa883d0e2a3502a60058cbceeb953
vkrajsa <vkrajsa@gmail.com>
2022-03-01 10:26:59 +0100

    Add article edit table row

---
+++ src/components/ArticleTableRow.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import BaseButton from './base/BaseButton.vue';
+import { Article } from '@/types/article';
+import { blogAuthor } from '../composable/useArticles';
+import { routerPush } from '@/router/index';
+
+interface Props {
+  article: Article;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <tr>
+    <td>{{ props.article.title }}</td>
+    <td>{{ props.article.perex }}</td>
+    <td>{{ blogAuthor }}</td>
+    <td>
+      <BaseButton>EDIT</BaseButton>
+      <BaseButton>DELETE</BaseButton>
+    </td>
+  </tr>
+</template>
