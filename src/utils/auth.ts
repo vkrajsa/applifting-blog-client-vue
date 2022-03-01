94ce95c56d97348485545c26259651ce24e213a0
vkrajsa <vkrajsa@gmail.com>
2022-03-01 09:36:23 +0100

    Return token only if exists in localstorage

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { userStorage } from '@/store/modules/user';
 
-export function setAuthorizationHeader(): string {
+export function setAuthorizationHeader(): string | undefined {
   const token = userStorage.get();
-  return `Bearer ${token.access_token}`;
+  if (token) return `Bearer ${token.access_token}`;
 }
