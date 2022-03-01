ffff18aecaa6e59332a5a87536bebf8c6ce67263
vkrajsa <vkrajsa@gmail.com>
2022-03-01 14:00:01 +0100

    Render admin dashboard table

--- src/views/ArticlesAdmin.vue
+++ src/views/ArticlesAdmin.vue
@@ -1,7 +1,7 @@
-<template>
-  <div class="home">
-    <h1>My Articles</h1>
-  </div>
-</template>
+<script setup lang="ts">
+import ArticleTable from '../components/ArticleTable.vue';
+</script>
 
-<script></script>
+<template>
+  <ArticleTable />
+</template>
