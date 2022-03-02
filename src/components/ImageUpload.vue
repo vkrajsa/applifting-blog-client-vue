d6e4d5f32c7f826bc6b5b66e8d1e422267b22b8e
vkrajsa <vkrajsa@gmail.com>
2022-03-02 09:30:20 +0100

    Add upload image component

---
+++ src/components/ImageUpload.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="row mb-3 justify-content-start">
+    <div class="col-8">
+      <input
+        type="file"
+        class="form-control"
+        @input="$emit('get-file', $event.target.files[0])"
+        @change="setImgPreview"
+        required
+      />
+    </div>
+    <div class="row justify-content-start my-3">
+      <div class="col-4 justify-content-end">
+        <div class="card border d-flex aligns-items-center justify-content-center">
+          <span v-if="!imgPreview" class="text-center"> Your image</span>
+          <img alt="Img preview" width="150" height="150" :src="imgPreview" v-show="imgPreview" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    required: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      imgPreview: null,
+    };
+  },
+  methods: {
+    setImgPreview(event) {
+      this.imgPreview = URL.createObjectURL(event.target.files[0]);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.card {
+  width: 150px;
+  height: 150px;
+}
+</style>
