48d837fa0e0b648178a37dd5c0a6412ed07afa44
vkrajsa <vkrajsa@gmail.com>
2022-03-03 15:09:40 +0100

    Add base spinner

---
+++ src/components/base/BaseLoader.vue
@@ -0,0 +1,11 @@
+<script>
+props: ['color'];
+</script>
+
+<template>
+  <div class="d-flex justify-content-center">
+    <div class="spinner-border" :color="color">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+</template>
