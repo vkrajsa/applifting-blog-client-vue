f27af2f70961772a27f14190f799fa55b9f1b02b
vkrajsa <vkrajsa@gmail.com>
2022-03-04 11:56:18 +0100

    Add loader option to the base button

--- src/components/base/BaseLoader.vue
+++ src/components/base/BaseLoader.vue
@@ -1,11 +1,26 @@
 <script>
 export default {
-  props: ['color'],
+  props: {
+    color: {
+      type: String,
+      default: null,
+    },
+    button: {
+      type: Boolean,
+      default: true,
+    },
+  },
 };
 </script>
 
 <template>
-  <div class="d-flex justify-content-center">
+  <div v-if="button" class="d-inline justify-content-center">
+    <div class="spinner-border spinner-border-sm me-2" :class="color">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+
+  <div v-else class="d-flex justify-content-center">
     <div class="spinner-border" :class="color">
       <span class="visually-hidden">Loading...</span>
     </div>
